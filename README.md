bd7a3c97f209c4ada0e52d00d86a9dbbfda750e9
Ellison <jiahui_tsui@163.com>
2017-08-16 17:53:32 +0800

    add js class01

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # tech_web
-
-# 17/08/16
-    test README.md
+## tutorial section01:javascript
+    <ol>
+        <li><a href="http://localhost:8082/test01">JavaScript动态效果初体验</li>
+    </ol>
--- tech_web.py
+++ tech_web.py
@@ -1,16 +1,17 @@
 # -*- coding:utf-8 -*-
-from flask import Flask, render_template, session, url_for, redirect
+from flask import Flask, render_template, url_for, redirect
 
 app = Flask(__name__)
 
-
+# tutorial section 01: javascript
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return redirect(url_for('js_test01')) #url_for的参数要是路径对应的“方法名”
 
-@app.route('/test')
-def test():
-    return render_template('test.html')
+@app.route('/test01')
+def js_test01():
+    # 首次在前端添加javascript的效果提示
+    return render_template('test01.html')
 
 if __name__ == '__main__':
     app.debug = True
